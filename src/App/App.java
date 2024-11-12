92c0454322fed25e4e395987896bf8412b07eb91
JulianKonowalski <julian.konwalski@gmail.com>
2024-11-12 08:15:23 +0100

    separated simulation and graphics components

--- src/App/App.java
+++ src/App/App.java
@@ -1,23 +1,55 @@
 package App;
 
+import App.Simulation.Simulation;
 import App.Util.Logger;
+import App.Util.Timer;
+import java.util.concurrent.TimeUnit;
 
-public class App {
+public class App implements Runnable {
 
-    public App(String windowTitle, int windowWidth, int windowHeight, int FPS, String logFilePath, String logDateFormat) {
-        mWindow = new Window();
-        mPanel = mWindow.setup(windowTitle, windowWidth, windowHeight, FPS);
-        mLogger = new Logger(logFilePath, logDateFormat);
+  public App(String windowTitle, int windowWidth, int windowHeight, int FPS, String logFilePath, String logDateFormat) {
+    mTimestep = 1.0 / FPS;
+    mWindow = new Window();
+    mPanel = mWindow.setup(windowTitle, windowWidth, windowHeight, FPS);
+    mLogger = new Logger(logFilePath, logDateFormat);
+    mTimer = new Timer();
+    mSimulation = new Simulation(mTimestep);
+    mSimulationThread = new Thread(this);
+  }
+
+  @Override
+  public void run() {
+    mLogger.log("App", "Started the app");
+    mTimer.start();
+
+    while (mSimulationThread != null) { //MAIN LOOP
+      mPanel.resolveMouseEvents();
+      mSimulation.update();
+      mPanel.drawScene(mSimulation.getState().getBodies());
+      this.frameSync();
+      mTimer.start();
     }
 
-    public void run() {
-        mLogger.log("App", "Started the app");
-        mPanel.startSimulation();
-        mLogger.log("App", "Closed the app\n");
-        mLogger.close();
-    }
+    mLogger.log("App", "Closed the app\n");
+    mLogger.close();
+  }
 
-    private final Window mWindow;
-    private final SimulationPanel mPanel;
-    private final Logger mLogger;
+  private void frameSync() { //delay a frame to match the target FPS
+    try {
+      long sleepTime = (long) (mTimestep * 1e9) - mTimer.getElapsedTime();
+      if(sleepTime < 0) { throw new IllegalStateException("Simulation is running too slow"); }
+      TimeUnit.NANOSECONDS.sleep(sleepTime);
+    } catch (InterruptedException e) {
+      System.out.println(e.getMessage());
+      System.exit(0);
+    }
+  }
+
+  private final double mTimestep;
+  private final Window mWindow;
+  private final SimulationPanel mPanel;
+  private final Logger mLogger;
+  private final Timer mTimer;
+  private final Simulation mSimulation;
+  private final Thread mSimulationThread;
 }
